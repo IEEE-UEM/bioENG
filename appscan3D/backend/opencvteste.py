d849324b13ef74bb3d6a15dfea4d913c05856c2f
Luan S. P. Crozatti <55167412+LCrozatti@users.noreply.github.com>
2020-06-23 13:50:18 -0300

    Update opencvteste.py

--- appscan3D/backend/opencvteste.py
+++ appscan3D/backend/opencvteste.py
@@ -1,30 +1,28 @@
 # Baixar o pip
 # Baixar OpenCV
 
-# O que é OpenCV e o que ele é capaz de fazer
-
-# Demonstrar um código simples
+# Demonstrar um codigo simples
 
 # Importar biblioteca
 import cv2
 
-# Carregar a imagem em uma variável
+# Carregar a imagem em uma variavel
 img = cv2.imread('mao2.jpeg',1)
-# 1 = Carrega a imagem com as cores padrões
+# 1 = Carrega a imagem com as cores padroes
 # 0 = Carrega a imagem em escala de cinza*
-# -1 = Alpha Channel = Mapa de transparência, relacionado a imagens vetorizadas (quadriculado no fundo)
+# -1 = Alpha Channel = Mapa de transparencia, relacionado a imagens vetorizadas (quadriculado no fundo)
 
 print(img)
-# Ṕrinta a matrizes com todos os pixels
+# printa matrizes com todos os pixels
 
 '''link para ver a cor de cada pixel https://www.w3schools.com/colors/colors_rgb.asp '''
 cv2.imshow('image', img)
 #imshow('titulodaimagem',variavel)
 
-cv2.waitKey(0) # = 0 imagem ficam congelada | > 0, tempo em ms para a imagem ficar aberta
-cv2.destroyAllWindows() # fecha todas as janelas após o tempo determinado
+cv2.waitKey(0) # = 0 imagem fica congelada, se >0 e o tempo em ms para a imagem ficar aberta
+cv2.destroyAllWindows() # fecha todas as janelas apos o tempo determinado
 
-cv2.imwrite('mao2p.png', img) #Faz uma cópia da variável img em .png
+cv2.imwrite('mao2p.png', img) #Faz uma copia da variavel img em .png
 
 # Citar apostila que revisa os algoritmos anteriores que realizam a 
-# conversão 2D --> 3D
+# conversao 2D --> 3D
